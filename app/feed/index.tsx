affd913c21240218efd39112c0cbc1ecbfa1e6c0
johnkamauwamunga <johnkamauwamunga@gmail.com>
2026-06-25 18:44:13 +0300

    restructuring props and components

--- app/feed/index.tsx
+++ app/feed/index.tsx
@@ -1,7 +1,7 @@
+import FeedCard from "@/components/indata/card";
 import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Posts from "../../assets/images/data/posts.json";
-import FeedCard from "./card";
 export default function Feed() {
   return (
     <SafeAreaView className="flex-1 bg-white">
