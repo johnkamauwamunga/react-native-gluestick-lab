c1579cca8f2b0c314257f67423223c70ed6ca6bf
johnkamauwamunga <johnkamauwamunga@gmail.com>
2026-06-27 03:22:53 +0300

    Updated Flatlist and evets display

--- app/articles/articles.tsx
+++ app/articles/articles.tsx
@@ -1,25 +1,33 @@
+import events from "@/assets/images/data/events.json";
 import EventCard from "@/components/events/eventCard";
 import Menu from "@/components/events/menu";
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 
 export default function Articles() {
   return (
     <View className="flex-1 bg-blue-700">
-      <Box className=" justify-center items-center h-[20%] w-full">
+      <Box className="justify-center items-center h-[20%] w-full">
         <Text className="font-bold text-xl color-white">The standard</Text>
       </Box>
       <Box
-        className="bg-white h-full"
+        className="bg-white flex-1" // <-- added flex-1 to fill remaining space
         style={{
           borderTopLeftRadius: 40,
           borderTopRightRadius: 40,
         }}
       >
         <Menu />
-        <Box>
-          <EventCard />
+        <Box className="flex-1">
+          {" "}
+          {/* <-- this Box takes the rest of the height */}
+          <FlatList
+            data={events}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <EventCard event={item} />}
+            showsVerticalScrollIndicator={false}
+          />
         </Box>
       </Box>
     </View>
